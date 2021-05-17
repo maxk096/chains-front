253a14fdf05506041e592911d8bab4c8b5a8615e
Max Kanaradze <33376471+null096@users.noreply.github.com>
2021-05-17 20:46:22 +0300

    add enableArea to line score chart

--- src/components/habit-details/charts/score-chart.js
+++ src/components/habit-details/charts/score-chart.js
@@ -165,6 +165,7 @@ class ScoreChartPure extends React.Component {
                                 pointBorderColor={{ from: 'serieColor' }}
                                 pointLabelYOffset={-12}
                                 useMesh={true}
+                                enableArea
                             />
                         )}
                     </AutoSizer>
